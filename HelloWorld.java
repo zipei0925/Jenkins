6befab86421e91f55b8df9af6a08d0329ebf55d9
zipei0925 <425898860@qq.com>
2021-06-29 14:51:41 +0800

    v2

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,8 @@ public class HelloWorld{
 	public static void main(String[] args){
 		for(int i=0; i<10; i++){
 			System.out.println("Hello World" + i);
+			System.out.println("Hello Jenkins" + i);
+
 		}
 		System.out.println("build succeed");
 	}
